8a0ad00af50ab9dd75a17ce7315a04af8dd5d91d
solivekeranwork-spec <solivekeran.work@gmail.com>
2026-06-12 17:48:16 +0530

    Revert "feat: add Tamil language version of chatbot (#6)"
    This reverts commit 30506819ee7af772d6ff5f8a40aa9f083289e59a.

--- preprocess_tamil.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def preprocess(text: str) -> str:
-    text = text.strip()
-    text = re.sub(r'\s+', ' ', text)
-    # Remove punctuation but keep Tamil characters, digits, dots, and spaces
-    text = re.sub(r"[^\u0B80-\u0BFF\w\s\.]", ' ', text)
-    return text
